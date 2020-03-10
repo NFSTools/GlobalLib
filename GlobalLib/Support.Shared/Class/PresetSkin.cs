edfc11cf601344a9a4bb9bdc2ce021052309ec87
maxhwoy <max.hwoy@gmail.com>
2020-03-10 01:28:03 -0700

    Combined Shared PresetSkin and STRBlock into ones.

---
+++ GlobalLib/Support.Shared/Class/PresetSkin.cs
@@ -0,0 +1,117 @@
+﻿namespace GlobalLib.Support.Shared.Class
+{
+    public partial class PresetSkin : Reflection.Abstract.ICollectable
+    {
+        #region Private Fields
+
+        private string _painttype = Reflection.BaseArguments.PPAINT;
+        private byte _paintswatch = 1;
+        private float _paintsaturation = 0;
+        private float _paintbrightness = 0;
+
+        #endregion
+
+        #region Main Properties
+
+        /// <summary>
+        /// Collection name of the variable.
+        /// </summary>
+        public override string CollectionName { get; set; }
+
+        /// <summary>
+        /// Binary memory hash of the collection name.
+        /// </summary>
+        public virtual uint BinKey { get => Utils.Bin.Hash(this.CollectionName); }
+
+        /// <summary>
+        /// Vault memory hash of the collection name.
+        /// </summary>
+        public virtual uint VltKey { get => Utils.Vlt.Hash(this.CollectionName); }
+
+        #endregion
+
+        #region AccessModifiable Properties
+
+        /// <summary>
+        /// Paint type value of the preset skin.
+        /// </summary>
+        [Reflection.Attributes.AccessModifiable()]
+        public string PaintType
+        {
+            get => this._painttype;
+            set
+            {
+                if (Core.Map.BinKeys.ContainsValue(value))
+                    this._painttype = value;
+                else
+                    throw new Reflection.Exception.MappingFailException();
+            }
+        }
+
+        /// <summary>
+        /// Gradient color value of the paint of the preset skin. Range: 0-90.
+        /// </summary>
+        [Reflection.Attributes.AccessModifiable()]
+        public byte PaintSwatch
+        {
+            get => this._paintswatch;
+            set
+            {
+                if (value > 90)
+                    throw new System.ArgumentOutOfRangeException("Value passed should be in range 0 to 90.");
+                else
+                    this._paintswatch = value;
+            }
+        }
+
+        /// <summary>
+        /// Saturation value of the paint of the preset skin. Range: (float)0-1.
+        /// </summary>
+        [Reflection.Attributes.AccessModifiable()]
+        public float PaintSaturation
+        {
+            get => this._paintsaturation;
+            set
+            {
+                if (value > 1 || value < 0)
+                    throw new System.ArgumentOutOfRangeException("Value passed should be in range 0 to 1.");
+                else
+                    this._paintsaturation = value;
+            }
+        }
+
+        /// <summary>
+        /// Brightness value of the paint of the preset skin. Range: (float)0-1.
+        /// </summary>
+        [Reflection.Attributes.AccessModifiable()]
+        public float PaintBrightness
+        {
+            get => this._paintbrightness;
+            set
+            {
+                if (value > 1 || value < 0)
+                    throw new System.ArgumentOutOfRangeException("Value passed should be in range 0 to 1.");
+                else
+                    this._paintbrightness = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Assembles preset skin into a byte array.
+        /// </summary>
+        /// <returns>Byte array of the preset skin.</returns>
+        public virtual unsafe byte[] Assemble() { return null; }
+
+        /// <summary>
+        /// Disassembles preset skin array into separate properties.
+        /// </summary>
+        /// <param name="byteptr_t">Pointer to the preset skin array.</param>
+        protected virtual unsafe void Disassemble(byte* byteptr_t) { }
+
+        #endregion
+    }
+}
---
+++ GlobalLib/Support.Shared/Class/STRBlock.cs
@@ -0,0 +1,47 @@
+﻿namespace GlobalLib.Support.Shared.Class
+{
+	public class STRBlock
+	{
+		#region Main Properties
+
+		/// <summary>
+		/// Binary memory hash of the collection name.
+		/// </summary>
+		public virtual uint BinKey { get => 0; }
+
+        /// <summary>
+        /// Vault memory hash of the collection name.
+        /// </summary>
+        public virtual uint VltKey { get => 0; }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Assembles string block into a byte array.
+		/// </summary>
+		/// <returns>Byte array of the string block.</returns>
+		public virtual unsafe byte[] Assemble() { return null; }
+
+		/// <summary>
+		/// Disassembles string block array into separate properties.
+		/// </summary>
+		/// <param name="byteptr_t">Pointer to the string block array.</param>
+		protected virtual unsafe void Disassemble(byte* byteptr_t, int length) { }
+
+		/// <summary>
+		/// Disassembles labels block array into separate properties.
+		/// </summary>
+		/// <param name="byteptr_t">Pointer to the label block array.</param>
+		protected virtual unsafe void DisperseLabels(byte* byteptr_t, int length) { }
+
+		/// <summary>
+		/// Assembles labels block into a byte array.
+		/// </summary>
+		/// <returns>Byte array of the labels block.</returns>
+		public virtual unsafe byte[] ParseLabels() { return null; }
+
+		#endregion
+	}
+}
