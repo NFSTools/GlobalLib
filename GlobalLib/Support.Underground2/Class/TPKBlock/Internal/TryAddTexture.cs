172cbacc131c5862f84c728bab4bf507f3b2770d
maxhwoy <max.hwoy@gmail.com>
2020-04-02 18:16:16 -0700

    Changed texture methods for Underground2 TPKBlocks.

--- GlobalLib/Support.Underground2/Class/TPKBlock/Internal/TryAddTexture.cs
+++ GlobalLib/Support.Underground2/Class/TPKBlock/Internal/TryAddTexture.cs
@@ -3,22 +3,53 @@
     public partial class TPKBlock
     {
         /// <summary>
-        /// Attempts to add texture to the TPKBlock data.
+        /// Attempts to add <see cref="Texture"/> to the <see cref="TPKBlock"/> data.
         /// </summary>
-        /// <param name="CName">Collection Name of the new texture.</param>
+        /// <param name="CName">Collection Name of the new <see cref="Texture"/>.</param>
         /// <param name="filename">Path of the texture to be imported.</param>
         /// <returns>True if texture adding was successful, false otherwise.</returns>
         public override bool TryAddTexture(string CName, string filename)
         {
             if (string.IsNullOrWhiteSpace(CName)) return false;
 
-            if (this.GetTextureIndex(CName) != -1)
+            if (this.FindTexture(Utils.Bin.Hash(CName), Reflection.Enum.eKeyType.BINKEY) != null)
                 return false;
-
-            if (CName.Length > 0x17) return false;
 
             if (!Utils.EA.Comp.IsDDSTexture(filename))
                 return false;
+
+            var texture = new Texture(CName, this.CollectionName, filename, this.Database);
+            this.Textures.Add(texture);
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to add <see cref="Texture"/> to the <see cref="TPKBlock"/> data.
+        /// </summary>
+        /// <param name="CName">Collection Name of the new <see cref="Texture"/>.</param>
+        /// <param name="filename">Path of the texture to be imported.</param>
+        /// <param name="error">Error occured when trying to add a texture.</param>
+        /// <returns>True if texture adding was successful, false otherwise.</returns>
+        public override bool TryAddTexture(string CName, string filename, out string error)
+        {
+            error = null;
+            if (string.IsNullOrWhiteSpace(CName))
+            {
+                error = $"Collection Name cannot be empty or whitespace.";
+                return false;
+            }
+
+            if (this.FindTexture(Utils.Bin.Hash(CName), Reflection.Enum.eKeyType.BINKEY) != null)
+            {
+                error = $"Texture named {CName} already exists.";
+                return false;
+            }
+
+            if (!Utils.EA.Comp.IsDDSTexture(filename))
+            {
+                error = $"Texture passed is not a DDS texture.";
+                return false;
+            }
 
             var texture = new Texture(CName, this.CollectionName, filename, this.Database);
             this.Textures.Add(texture);
--- GlobalLib/Support.Underground2/Class/TPKBlock/Internal/TryRemoveTexture.cs
+++ GlobalLib/Support.Underground2/Class/TPKBlock/Internal/TryRemoveTexture.cs
@@ -3,16 +3,35 @@
     public partial class TPKBlock
     {
         /// <summary>
-        /// Attempts to remove texture specified from TPKBlock data.
+        /// Attempts to remove <see cref="Texture"/> specified from <see cref="TPKBlock"/> data.
         /// </summary>
-        /// <param name="CName">Collection Name of the texture to be deleted.</param>
+        /// <param name="key">Key of the Collection Name of the <see cref="Texture"/> to be deleted.</param>
+        /// <param name="type">Type fo the key passed.</param>
         /// <returns>True if texture removing was successful, false otherwise.</returns>
-        public override bool TryRemoveTexture(string CName)
+        public override bool TryRemoveTexture(uint key, Reflection.Enum.eKeyType type)
         {
-            int index = this.GetTextureIndex(CName);
-            if (index == -1)
-                return false;
+            var index = this.GetTextureIndex(key, type);
+            if (index == -1) return false;
+            this.Textures.RemoveAt(index);
+            return true;
+        }
 
+        /// <summary>
+        /// Attempts to remove <see cref="Texture"/> specified from <see cref="TPKBlock"/> data.
+        /// </summary>
+        /// <param name="key">Key of the Collection Name of the <see cref="Texture"/> to be deleted.</param>
+        /// <param name="type">Type of the key passed.</param>
+        /// <param name="error">Error occured when trying to remove a texture.</param>
+        /// <returns>True if texture removing was successful, false otherwise.</returns>
+        public override bool TryRemoveTexture(uint key, Reflection.Enum.eKeyType type, out string error)
+        {
+            error = null;
+            var index = this.GetTextureIndex(key, type);
+            if (index == -1)
+            {
+                error = $"Texture with key 0x{key:X8} does not exist.";
+                return false;
+            }
             this.Textures.RemoveAt(index);
             return true;
         }
