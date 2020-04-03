3bf7c852968ea59bcd807d12bfdce013db695064
maxhwoy <max.hwoy@gmail.com>
2020-04-02 18:12:48 -0700

    Changed texture methods for MostWanted TPKBlock.

--- GlobalLib/Support.MostWanted/Class/TPKBlock/Internal/TryExportTexture.cs
+++ GlobalLib/Support.MostWanted/Class/TPKBlock/Internal/TryExportTexture.cs
@@ -3,46 +3,44 @@
     public partial class TPKBlock
     {
         /// <summary>
-        /// Attemps to export texture specified to the path and mode provided.
+        /// Attemps to export <see cref="Texture"/> specified to the path and mode provided.
         /// </summary>
-        /// <param name="CName">Collection Name of the texture to be exported.</param>
+        /// <param name="key">Key of the Collection Name of the <see cref="Texture"/> to be exported.</param>
+        /// <param name="type">Type of the key passed.</param>
         /// <param name="path">Path where the texture should be exported.</param>
         /// <param name="mode">Mode in which export the texture. Range: ".dds", ".png", ".jpg", ".tiff", ".bmp".</param>
         /// <returns>True if texture export was successful, false otherwise.</returns>
-        public override bool TryExportTexture(string CName, string path, string mode)
+        public override bool TryExportTexture(uint key, Reflection.Enum.eKeyType type,
+            string path, string mode)
         {
-            DevIL.ImageType type;
+            DevIL.ImageType ddstype;
             switch (mode)
             {
                 case ".dds":
-                    type = DevIL.ImageType.Dds;
+                    ddstype = DevIL.ImageType.Dds;
                     break;
                 case ".png":
-                    type = DevIL.ImageType.Png;
+                    ddstype = DevIL.ImageType.Png;
                     break;
                 case ".jpg":
-                    type = DevIL.ImageType.Jpg;
+                    ddstype = DevIL.ImageType.Jpg;
                     break;
                 case ".tiff":
-                    type = DevIL.ImageType.Tiff;
+                    ddstype = DevIL.ImageType.Tiff;
                     break;
                 case ".bmp":
-                    type = DevIL.ImageType.Bmp;
+                    ddstype = DevIL.ImageType.Bmp;
                     break;
                 default:
-                    if (Core.Process.MessageShow)
-                        System.Windows.Forms.MessageBox.Show("Export mode provided is not supported.", "Warning");
-                    else
-                        System.Console.WriteLine("Export mode provided is not supported.");
                     return false;
             }
 
-            int index = this.GetTextureIndex(CName);
-            if (index == -1) return false;
+            var tex = (Texture)this.FindTexture(key, type);
+            if (tex == null) return false;
 
             try
             {
-                var data = this.Textures[index].GetDDSArray();
+                var data = tex.GetDDSArray();
                 if (mode == ".dds")
                 {
                     using (var bw = new System.IO.BinaryWriter(System.IO.File.Open(path, System.IO.FileMode.Create)))
@@ -58,7 +56,77 @@
                     {
                         using (var image = im.LoadImageFromStream(sr))
                         {
-                            ex.SaveImage(image, type, path);
+                            ex.SaveImage(image, ddstype, path);
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (System.Exception) { return false; }
+        }
+
+        /// <summary>
+        /// Attemps to export <see cref="Texture"/> specified to the path and mode provided.
+        /// </summary>
+        /// <param name="key">Key of the Collection Name of the <see cref="Texture"/> to be exported.</param>
+        /// <param name="type">Type of the key passed.</param>
+        /// <param name="path">Path where the texture should be exported.</param>
+        /// <param name="mode">Mode in which export the texture. Range: ".dds", ".png", ".jpg", ".tiff", ".bmp".</param>
+        /// <param name="error">Error occured when trying to clone a texture.</param>
+        /// <returns>True if texture export was successful, false otherwise.</returns>
+        public override bool TryExportTexture(uint key, Reflection.Enum.eKeyType type,
+            string path, string mode, out string error)
+        {
+            error = null;
+            DevIL.ImageType ddstype;
+            switch (mode)
+            {
+                case ".dds":
+                    ddstype = DevIL.ImageType.Dds;
+                    break;
+                case ".png":
+                    ddstype = DevIL.ImageType.Png;
+                    break;
+                case ".jpg":
+                    ddstype = DevIL.ImageType.Jpg;
+                    break;
+                case ".tiff":
+                    ddstype = DevIL.ImageType.Tiff;
+                    break;
+                case ".bmp":
+                    ddstype = DevIL.ImageType.Bmp;
+                    break;
+                default:
+                    error = $"{mode} is not a supported image type that can be exported.";
+                    return false;
+            }
+
+            var tex = (Texture)this.FindTexture(key, type);
+            if (tex == null)
+            {
+                error = $"Texture with key 0x{key:X8} does not exist.";
+                return false;
+            }
+
+            try
+            {
+                var data = tex.GetDDSArray();
+                if (mode == ".dds")
+                {
+                    using (var bw = new System.IO.BinaryWriter(System.IO.File.Open(path, System.IO.FileMode.Create)))
+                    {
+                        bw.Write(data);
+                    }
+                }
+                else
+                {
+                    using (var sr = new System.IO.MemoryStream(data))
+                    using (var im = new DevIL.ImageImporter())
+                    using (var ex = new DevIL.ImageExporter())
+                    {
+                        using (var image = im.LoadImageFromStream(sr))
+                        {
+                            ex.SaveImage(image, ddstype, path);
                         }
                     }
                 }
@@ -66,10 +134,8 @@
             }
             catch (System.Exception e)
             {
-                if (Core.Process.MessageShow)
-                    System.Windows.Forms.MessageBox.Show(e.Message, "Warning");
-                else
-                    System.Console.WriteLine($"{e.Message}");
+                while (e.InnerException != null) e = e.InnerException;
+                error = e.Message;
                 return false;
             }
         }
