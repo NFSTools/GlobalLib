d58d5f75c9b83b0a2d47a3a0130f44d506521af9
maxhwoy <max.hwoy@gmail.com>
2020-04-03 11:34:46 -0700

    Moved all color methods to Shared FNGroup.

--- GlobalLib/Support.Shared/Class/FNGroup.cs
+++ GlobalLib/Support.Shared/Class/FNGroup.cs
@@ -10,30 +10,33 @@ namespace GlobalLib.Support.Shared.Class
         #region Main Properties
 
         /// <summary>
-        /// Collection name of the variable.
+        /// Collection Name of the class.
         /// </summary>
         public string CollectionName { get; protected set; }
 
         /// <summary>
-        /// Binary memory hash of the collection name.
+        /// Binary memory hash of the Collection Name.
         /// </summary>
         public uint BinKey { get => Utils.Bin.Hash(this.CollectionName); }
 
         /// <summary>
-        /// Vault memory hash of the collection name.
+        /// Vault memory hash of the Collection Name.
         /// </summary>
         public uint VltKey { get => Utils.Vlt.Hash(this.CollectionName); }
 
         /// <summary>
-        /// Size of the group in Global memory.
+        /// Size of the <see cref="FNGroup"/> in Global memory.
         /// </summary>
         public uint Size { get; protected set; }
 
         /// <summary>
-        /// Represents boolean of whether this group can be destroyed b/c it is repetitive.
+        /// Represents boolean of whether this <see cref="FNGroup"/> can be destroyed b/c it is repetitive.
         /// </summary>
         public bool Destroy { get; protected set; } = false;
 
+        /// <summary>
+        /// List of all <see cref="FEngColor"/> that <see cref="FNGroup"/> contains.
+        /// </summary>
         protected List<FEngColor> _colorinfo = new List<FEngColor>();
 
         /// <summary>
@@ -69,17 +72,6 @@ namespace GlobalLib.Support.Shared.Class
         }
 
         /// <summary>
-        /// Gets <see cref="FEngColor"/> using Color[X] string.
-        /// </summary>
-        /// <param name="colortext">String of Color[X] format.</param>
-        /// <returns><see cref="FEngColor"/> class.</returns>
-        public virtual FEngColor GetColor(string colortext)
-        {
-            if (!Utils.FormatX.GetInt32(colortext, "Color[{X}]", out int index)) return null;
-            else return this.GetColor(index);
-        }
-
-        /// <summary>
         /// Attempts to set new <see cref="FEngColor"/> at specific color.
         /// </summary>
         /// <param name="index">Index of the color to set.</param>
@@ -88,23 +80,6 @@ namespace GlobalLib.Support.Shared.Class
         public virtual bool TrySetOne(int index, FEngColor color)
         {
             var thiscolor = this.GetColor(index);
-            if (thiscolor == null) return false;
-            thiscolor.Alpha = color.Alpha;
-            thiscolor.Red = color.Red;
-            thiscolor.Green = color.Green;
-            thiscolor.Blue = color.Blue;
-            return true;
-        }
-
-        /// <summary>
-        /// Attempts to set new <see cref="FEngColor"/> at specific color.
-        /// </summary>
-        /// <param name="colortext">String of Color[X] format.</param>
-        /// <param name="color">New <see cref="FEngColor"/> to set.</param>
-        /// <returns>True if setting new color was successful; false otherwise.</returns>
-        public virtual bool TrySetOne(string colortext, FEngColor color)
-        {
-            var thiscolor = this.GetColor(colortext);
             if (thiscolor == null) return false;
             thiscolor.Alpha = color.Alpha;
             thiscolor.Red = color.Red;
@@ -137,42 +112,109 @@ namespace GlobalLib.Support.Shared.Class
         }
 
         /// <summary>
-        /// Attempts to set new <see cref="FEngColor"/> at specific color.
+        /// Attempts to set same <see cref="FEngColor"/> to a specific color.
         /// </summary>
-        /// <param name="colortext">String of Color[X] format.</param>
-        /// <param name="color">New <see cref="FEngColor"/> to set.</param>
-        /// <param name="error">Error occured when trying to set one color.</param>
-        /// <returns>True if setting new color was successful; false otherwise.</returns>
-        public virtual bool TrySetOne(string colortext, FEngColor color, out string error)
+        /// <param name="index">Index of the <see cref="FEngColor"/> to match.</param>
+        /// <param name="newbase">New <see cref="FEngColor"/> to set.</param>
+        /// <param name="keepalpha">True if alpha value should be kept; false otherwise.</param>
+        /// <returns>True if setting same colors was successful; false otherwise.</returns>
+        public virtual bool TrySetSame(int index, FEngColor newbase, bool keepalpha)
         {
-            error = null;
-            var thiscolor = this.GetColor(colortext);
-            if (thiscolor == null)
+            var excolor = this.GetColor(index);
+            if (excolor == null) return false;
+            var sample = new FEngColor()
             {
-                error = $"Unable to find color {colortext}.";
-                return false;
+                Alpha = excolor.Alpha,
+                Red = excolor.Red,
+                Green = excolor.Green,
+                Blue = excolor.Blue
+            };
+            foreach (var color in this._colorinfo)
+            {
+                if (color == sample)
+                {
+                    color.Red = sample.Red;
+                    color.Green = sample.Green;
+                    color.Blue = sample.Blue;
+                    if (!keepalpha) color.Alpha = sample.Alpha;
+                }
             }
-            thiscolor.Alpha = color.Alpha;
-            thiscolor.Red = color.Red;
-            thiscolor.Green = color.Green;
-            thiscolor.Blue = color.Blue;
             return true;
         }
 
-        public virtual bool TrySetSame(FEngColor sample, FEngColor newbase, bool keepalpha) { return false; }
-
-        public virtual bool TrySetSame(FEngColor sample, FEngColor newbase, bool keepalpha, out string error)
+        /// <summary>
+        /// Attempts to set same <see cref="FEngColor"/> to a specific color.
+        /// </summary>
+        /// <param name="index">Index of the <see cref="FEngColor"/> to match.</param>
+        /// <param name="newbase">New <see cref="FEngColor"/> to set.</param>
+        /// <param name="keepalpha">True if alpha value should be kept; false otherwise.</param>
+        /// <param name="error">Error occured when trying to set same colors.</param>
+        /// <returns>True if setting same colors was successful; false otherwise.</returns>
+        public virtual bool TrySetSame(int index, FEngColor newbase, bool keepalpha, out string error)
         {
             error = null;
-            return false;
+            var excolor = this.GetColor(index);
+            if (excolor == null)
+            {
+                error = $"Color with index {index} does not exist.";
+                return false;
+            }
+            var sample = new FEngColor()
+            {
+                Alpha = excolor.Alpha,
+                Red = excolor.Red,
+                Green = excolor.Green,
+                Blue = excolor.Blue
+            };
+            foreach (var color in this._colorinfo)
+            {
+                if (color == sample)
+                {
+                    color.Red = sample.Red;
+                    color.Green = sample.Green;
+                    color.Blue = sample.Blue;
+                    if (!keepalpha) color.Alpha = sample.Alpha;
+                }
+            }
+            return true;
         }
 
-        public virtual bool TrySetAll(FEngColor color, bool keepalpha) { return false; }
+        /// <summary>
+        /// Attempts to set all <see cref="FEngColor"/> to a specific color.
+        /// </summary>
+        /// <param name="color"><see cref="FEngColor"/> to set for all colors.</param>
+        /// <param name="keepalpha">True if alpha value should be kept; false otherwise.</param>
+        /// <returns>True if setting all colors was successful; false otherwise.</returns>
+        public virtual bool TrySetAll(FEngColor color, bool keepalpha)
+        {
+            foreach (var thiscolor in this._colorinfo)
+            {
+                thiscolor.Red = color.Red;
+                thiscolor.Green = color.Green;
+                thiscolor.Blue = color.Blue;
+                if (!keepalpha) thiscolor.Alpha = color.Alpha;
+            }
+            return true;
+        }
 
+        /// <summary>
+        /// Attempts to set all <see cref="FEngColor"/> to a specific color.
+        /// </summary>
+        /// <param name="color"><see cref="FEngColor"/> to set for all colors.</param>
+        /// <param name="keepalpha">True if alpha value should be kept; false otherwise.</param>
+        /// <param name="error">Error occured when trying to set all colors.</param>
+        /// <returns>True if setting all colors was successful; false otherwise.</returns>
         public virtual bool TrySetAll(FEngColor color, bool keepalpha, out string error)
         {
             error = null;
-            return false;
+            foreach (var thiscolor in this._colorinfo)
+            {
+                thiscolor.Red = color.Red;
+                thiscolor.Green = color.Green;
+                thiscolor.Blue = color.Blue;
+                if (!keepalpha) thiscolor.Alpha = color.Alpha;
+            }
+            return true;
         }
 
         #endregion
