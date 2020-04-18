7978dd7ba764fd6a65f014db7507e72d95e6b0bf
MaxHwoy <max.hwoy@gmail.com>
2020-04-18 04:01:52 -0700

    Revert changes back again.

--- GlobalLib/Support.Underground2/Class/TPKBlock/Internal/TPKBlockConstructor.cs
+++ GlobalLib/Support.Underground2/Class/TPKBlock/Internal/TPKBlockConstructor.cs
@@ -12,7 +12,7 @@ namespace GlobalLib.Support.Underground2.Class
         private List<uint> keys = new List<uint>(); // Part2
         private List<CompSlot> compressions = new List<CompSlot>(); // Part5
 
-        public TPKBlock() { this._use_current_cname = true; }
+        public TPKBlock() { this._use_current_cname = true; this.Version = 5; }
 
         public unsafe TPKBlock(byte* byteptr_t, int index, Database.Underground2 db)
         {
